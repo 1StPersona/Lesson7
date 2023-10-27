ddfc012d6a6bffc85ce3a2d715b3de87821448c6
Danila Zakharov <49668471+1StPersona@users.noreply.github.com>
2023-10-27 09:27:31 +0600

    Добавьте файлы проекта.

---
+++ Lesson7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks.DAL.WOT;
+
+namespace Lesson7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tank> t34 = new List<Tank>();
+            List<Tank> pantera = new List<Tank>();
+            string name1 = "t34-";
+            string name2 = "pantera-";
+            for (int i = 0; i < 5; i++)
+            {
+                t34.Add(new Tank(name1 + (i + 1).ToString()));
+                pantera.Add(new Tank(name2 + (i + 1).ToString()));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Бой {0}-ый: \n", i + 1);
+                t34[i].getInfo();
+                Console.WriteLine("\n----Против----\n");
+                pantera[i].getInfo();
+                Console.WriteLine("\n---Результат---\n");
+                Console.WriteLine("В {0} бою победил : " + (t34[i] * pantera[i]).getNaming() + "\n\n\n", i + 1);
+            }
+
+
+        }
+    }
+}
---
+++ Tanks.DAT.WOT/Tank.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks.DAL.WOT
+{
+    public class Tank
+    {
+
+        static Random rand = new Random();
+        private string Naming { get; set; }
+        private int Ammo { get; set; }
+        private int Armor { get; set; }
+        private int Mobility { get; set; }
+
+        public Tank() { }
+
+
+        public Tank(string naming)
+        {
+            this.Naming = naming;
+            this.Ammo = rand.Next(101);
+            this.Armor = rand.Next(101);
+            this.Mobility = rand.Next(102);
+        }
+
+        public string ReturnAmmo()
+        {
+            return this.Ammo.ToString();
+        }
+        public string ReturnArmor()
+        {
+            return this.Armor.ToString();
+        }
+        public string ReturnMobility()
+        {
+            return this.Mobility.ToString();
+        }
+
+        public static Tank operator *(Tank a, Tank b)
+        {
+            if (a.Ammo > b.Ammo && a.Armor > b.Armor ||
+               a.Mobility > b.Mobility && a.Armor > b.Armor)
+            {
+                return a;
+            }
+            else return b;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("Name: {0}\nAmmo: {1}\nArmor: {2}\nMobility: {3}", this.Naming, this.Ammo, this.Armor, this.Mobility);
+        }
+        public string getNaming()
+        {
+            return this.Naming;
+        }
+
+    }
+}
